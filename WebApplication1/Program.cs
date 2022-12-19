6d350a176748912a337b33bce9f77ee3c939b43d
hwgeone <15902138940@163.com>
2022-12-19 13:16:55 +0800

    test

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -17,7 +17,7 @@ namespace WebApplication1
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             //test
-            //nxjsnxsj
+            //karl huang
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
