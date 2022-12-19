b2584aadbadb92535dbea33df78753064964cf61
dan.dai <dan.dai@TCLAP6040>
2022-12-19 11:28:33 +0800

    111

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -16,7 +16,7 @@ namespace WebApplication1
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            //nxjsnxsj
+            //nxjsnxsj ds
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             ///mnmnmnm
             await builder.Build().RunAsync();
